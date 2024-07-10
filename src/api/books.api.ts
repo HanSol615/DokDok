51f895b3f18be460b9b7964834db4380ce02782f
박유진 <qkrdbw@naver.com>
2024-07-10 21:03:37 +0900

    Feat: book search api

--- src/api/books.api.ts
+++ src/api/books.api.ts
@@ -1,5 +1,9 @@
 import { Book } from "../models/model";
-import { httpClient } from "./http"
+import { httpClient } from "./http";
+
+interface SearchBooksParams {
+    title: string;
+};
 
 export const getFavoriteBooks = async () => {
     const response = await httpClient.get<Book[]>('/home/favorites');
@@ -15,4 +19,11 @@ export const getReadingBooks = async () => {
 export const getFinishedBooks = async () => {
     const response = await httpClient.get<Book[]>('/home/finishedBooks');
     return response.data || [];
+};
+
+export const searchBooks = async ({ title }: SearchBooksParams) => {
+    const response = await httpClient.get<Book[]>('/book/search', {
+        params: { title }
+    });
+    return response.data;
 };
