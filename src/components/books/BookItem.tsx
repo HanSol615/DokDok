60e532e347e659fc9727b622c866c4725a050ae6
박유진 <qkrdbw@naver.com>
2024-07-12 02:01:27 +0900

    Feature: add useBooks in BookItem component

--- src/components/books/BookItem.tsx
+++ src/components/books/BookItem.tsx
@@ -3,45 +3,69 @@ import { Book } from "../../models/model";
 import { Link } from 'react-router-dom';
 import { FaHeart } from 'react-icons/fa';
 import Button from "../common/Button";
+import { useBooks } from "../../hooks/useBooks";
 
 interface Props {
     book: Book;
     isFavoriteTab?: boolean;
 };
 
-const BookItem = ({ book }: Props) => {
-    const isFavoriteTab = true;
+const MAX_TITLE_LENGTH = 20;
+
+const truncateText = (text: string, maxLength: number) => {
+    if (text.length <= maxLength) {
+        return text;
+    } else {
+        return text.substring(0, maxLength) + '...';
+    }
+};
+
+const BookItem = ({ book, isFavoriteTab }: Props) => {
+    const truncatedTitle = truncateText(book.title, MAX_TITLE_LENGTH);
+    const { addFavorite, addReading, addFinished } = useBooks();
+
+    const handleFavoriteClick = () => {
+        addFavorite(book.isbn);
+    };
+
+    const handleReadingStartClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        const { title, author, image, publisher, isbn } = book;
+        addReading({ title, author, image, publisher, isbn });
+    };
+
+    const handleFinishedClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        addFinished(book.isbn);
+    };
 
     return (
         <Link to={`/books/${book.isbn}`}>
             <BookItemStyle>
                 <div className="img">
-                    <img src={`https://picsum.photos/id/${book.image}/200/200`} alt={book.title} />
+                    <img src={book.image} alt={book.title} />
                 </div>
                 <div className="content">
-                    <div>
-                        <h2 className="title">{book.title}</h2>
-                        <h3 className="author">{book.author}</h3>
+                    <div className="title-author">
+                        <h3 className="title">{truncatedTitle}</h3>
+                        <h4 className="author">{book.author}</h4>
                     </div>
                     <div className="buttons">
                         {isFavoriteTab ? (
-                            <Button size='small' scheme='primary'>
+                            <Button size='small' scheme='primary' onClick={handleReadingStartClick}>
                                 읽기 시작
                             </Button>
                         ) : (
                             book.readStatus === false ? (
-                                <Button size='small' scheme='primary'>
+                                <Button size='small' scheme='primary' onClick={handleFinishedClick}>
                                     읽기 완료
                                 </Button>
                             ) : (
                                 <div>평점</div>
                             )
                         )}
-                        {book.likeStatus && (
-                            <FaHeart />
-                        )}
+                        <FaHeart onClick={handleFavoriteClick}/>
                     </div>
-
                 </div>
             </BookItemStyle>
         </Link>
@@ -52,14 +76,21 @@ const BookItemStyle = styled.div`
     display: flex;
     flex-direction: row;
     gap: 2rem;
+    min-height: 15rem;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
     border-radius: 10px;
     padding: 25px;
 
+    .title-author {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
     .img {
         img {
             width: 9rem;
-            height: 11.5rem;
+            height: 12.3rem;
             border-radius: 0.2rem;
         }
     }
@@ -76,10 +107,8 @@ const BookItemStyle = styled.div`
 
     .buttons {
         display: flex;
-        justify-content: space-between;
-        gap: 25px;
+        gap: 20px;
         align-items: center;
-        margin-top: 10px;
 
         svg {
             font-size: 1.3rem;
