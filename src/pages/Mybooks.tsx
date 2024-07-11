0d4116069743e1f753f78105862aebcec9ccc79e
박유진 <qkrdbw@naver.com>
2024-07-12 02:06:57 +0900

    Feat: change response data

--- src/pages/Mybooks.tsx
+++ src/pages/Mybooks.tsx
@@ -4,18 +4,20 @@ import { Tab, Tabs } from "../components/common/Tabs";
 import { useBooks } from "../hooks/useBooks";
 
 const MyBooks = () => {
-    const { favoriteBooks, readingBooks, finishedBooks } = useBooks();
+    const { favoriteBooks, favoriteBooksCount, readingBooks, readingBooksCount, finishedBooks, finishedBooksCount } = useBooks();
+
+    console.log('즐겨찾기 책', favoriteBooks)
     return (
         <MyBooksStyle>
             <Tabs>
                 <Tab title="✨ 즐겨찾기">
-                    <Booklist books={favoriteBooks}/>
+                    <Booklist books={favoriteBooks} count={favoriteBooksCount} isFavoriteTab={true}/>
                 </Tab>
                 <Tab title="📖 읽는 중">
-                    <Booklist books={readingBooks}/>
+                    <Booklist books={readingBooks} count={readingBooksCount}/>
                 </Tab>
                 <Tab title="📚 읽은 책">
-                    <Booklist books={finishedBooks}/>
+                    <Booklist books={finishedBooks} count={finishedBooksCount}/>
                 </Tab>
             </Tabs>
         </MyBooksStyle>
