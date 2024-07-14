c8177fabd197796d31d37e0637c9d83f3cad4db9
박유진 <qkrdbw@naver.com>
2024-07-14 16:17:27 +0900

    Feat: book api

--- src/api/book.api.ts
+++ src/api/book.api.ts
@@ -1,7 +1,33 @@
-import { BookDetail } from "../models/model";
+import { wait } from "@testing-library/user-event/dist/utils";
+import { BookDetail, BookRemind, BookRemindResponse, BookReview, BookReviewResponse, ChangeBookDate, ChangeBookDateResponse, DeleteBookRemind } from "../models/model";
 import { httpClient } from "./http";
 
 export const getBookDetail = async (isbn: string) => {
     const response = await httpClient.get<BookDetail>('/book/detail', { params: { isbn: isbn } });
-    return response.data || [];
+    return response.data;
+};
+
+export const postBookReview = async (body: BookReview) => {
+    const response = await httpClient.post<BookReviewResponse>('/book/review', body);
+    return response.data;
+}
+
+export const postBookRemind = async (body: BookRemind) => {
+    const response = await httpClient.put<BookRemindResponse>('/book/remind', body);
+    return response.data;
+};
+
+export const deleteBook = async (isbn: string) => {
+    const response = await httpClient.delete('/book/deletebook', { data: { isbn: isbn } });
+    return response.data;
+};
+
+export const deleteBookRemind = async (body: DeleteBookRemind) => {
+    const response = await httpClient.delete('/book/deleteRemind', { data: body });
+    return response.data;
+};
+
+export const postChangeDate = async (body: ChangeBookDate) => {
+    const response = await httpClient.post<ChangeBookDateResponse>('/book/changeDate', { data: body });
+    return response.data;
 };
