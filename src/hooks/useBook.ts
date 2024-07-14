f34f67f996301a56f11907da3221afd99882188c
박유진 <qkrdbw@naver.com>
2024-07-14 16:18:56 +0900

    Feat: useBook hook

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -1,17 +1,57 @@
 import { useEffect, useState } from "react"
-import { BookDetail } from "../models/model";
-import { getBookDetail } from "../api/book.api";
+import { BookDetail, BookRemind, BookReview, BookReviewResponse, ChangeBookDate, ChangeBookDateResponse, DeleteBookRemind } from "../models/model";
+import { getBookDetail, postBookRemind, postBookReview, deleteBook, deleteBookRemind, postChangeDate } from "../api/book.api";
 
-export const useBook = (id: string | undefined) => {
-    const [bookDetail, setBookDetail] = useState<BookDetail | undefined>();
+export const useBook = (id: string) => {
+    const [bookDetail, setBookDetail] = useState<BookDetail | null>(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        if (id) {
-            getBookDetail(id).then((response) => {
-                setBookDetail(response);
-            });
-        }
+        const fetchBookDetail = async () => {
+            const response = await getBookDetail(id);
+            setBookDetail(response);
+            setLoading(false);
+        };
+
+        fetchBookDetail();
     }, [id]);
 
-    return { bookDetail };
+    const addReview = async (body: BookReview): Promise<BookReviewResponse> => {
+        try {
+            const response = await postBookReview(body);
+            alert('서평 작성이 완료되었습니다.');
+            return response;
+        } catch (error) {
+            console.error('서평 작성 실패:', error);
+            alert('서평 작성 중 오류가 발생했습니다.');
+            throw error;
+        }
+    };
+
+    const addRemind = (body: BookRemind) => {
+        postBookRemind(body).then(() => {
+            alert('구절 등록이 완료되었습니다.')
+        });
+    };
+
+    const removeBook = (isbn: string) => {
+        deleteBook(isbn).then(() => {
+            alert('서재에서 해당 책이 삭제되었습니다.');
+        });
+    };
+
+    const removeBookRemind = (body: DeleteBookRemind) => {
+        deleteBookRemind(body).then(() => {
+            alert('해당 구절이 삭제되었습니다.')
+        });
+    };
+
+    const changeDate = (body: ChangeBookDate) => {
+        postChangeDate(body).then((response: ChangeBookDateResponse) => {
+            alert('날짜가 변경되었습니다.')
+            return response;
+        });
+    };
+
+    return { bookDetail, loading, addReview, addRemind, removeBook, removeBookRemind, changeDate };
 };
